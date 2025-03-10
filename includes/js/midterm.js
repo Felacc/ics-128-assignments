b72f651bcc6b0745f1fcdf241ee0e3a2d645477e
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-10 11:10:54 -0700

    Refactor in the theme of SSBM

--- includes/js/midterm.js
+++ includes/js/midterm.js
@@ -1,10 +1,10 @@
 // Make User class using ES6
 class User {
-    constructor(firstName, lastName, email, password, isAdmin, profileImg) {
+    constructor(firstName, lastName, email, username, isAdmin, profileImg) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.password = password;
+        this.username = username;
         this.isAdmin = isAdmin;
         this.profileImg = profileImg;
 
@@ -22,26 +22,26 @@ class User {
 }
 
 // Admin Users
-const dobbsB = new User("Bob", "Dobbs", "dobbsb@churchofthesubgenius.org", "jrdobbs", true, "images/userimg.png");
-const mothersbaughM = new User("Mark", "Mothersbaugh", "mothersbaughm@churchofthesubgenius.org", "mothermarky", true, "images/userimg.png");
-const nixonM = new User("Mojo", "Nixon", "nixonm@churchofthesubgenius.org", "mrmojo", true, "images/userimg.png");
+const fox = new User("Fox", "McCloud", "marthistrash@fucknintendo.org", "fox", true, "images/midterm/fox.webp");
+const marth = new User("Marth", "", "fsmash@fucknintendo.org", "marth", true, "images/midterm/marth.webp");
+const jigglypuff = new User("jigglypuff", "", "sixjumps@fucknintendo.org", "jigglypuff", true, "images/midterm/jigglypuff.webp");
 
 // Regular Users
-const riftZ = new User("Zoogz", "Rift", "riftz@churchofthesubgenius.org", "zoogzy", false, "images/userimg.png");
-const mavridesP = new User("Paul", "Mavrides", "mavridesp@churchofthesubgenius.org", "paulm", false, "images/userimg.png");
-const reubensP = new User("Paul", "Reubens", "reubensp@churchthesubgenius.org", "reubdawg", false, "images/userimg.png");
-const byrneD = new User("David", "Byrne", "byrned@churchthesubgenius.org", "byrneysanders", false, "images/userimg.png");
-const crumbR = new User("R.", "Crumb", "crumbr@churchthesubgenius.org", "cookiecrumb", false, "images/userimg.png");
-const hoslerM = new User("Mark", "Hosler", "hoslerm@churchthesubgenius.org", "hosler", false, "images/userimg.png");
-const willsD = new User("David", "Wills", "willsd@churchthesubgenius.org", "theweatherman", false, "images/userimg.png");
-const conheimP = new User("Peter", "Conheim", "conheimp@churchthesubgenius.org", "conheim", false, "images/userimg.png");
-const leideckerJ = new User("Jon", "Leidecker", "leideckerj@churchthesubgenius.org", "wobbly", false, "images/userimg.png");
-const wilsonE = new User("Eric", "Wilson", "wilsone@churchthesubgenius.org", "sublimeeric", false, "images/userimg.png");
-const gaughB = new User("Bud", "Gaugh", "gaughb@churchthesubgenius.org", "sublimebud", false, "images/userimg.png");
-const nowellJ = new User("Jakob", "Nowell", "nowellj@churchthesubgenius.org", "sublimejakob", false, "images/userimg.png");
+const falco = new User("Falco", "Lombardi", "dair@fucknintendo.org", "falco", false, "images/midterm/falco.webp");
+const sheik = new User("Sheik", "", "notzelda@fucknintendo.org", "sheik", false, "images/midterm/sheik.webp");
+const captainFalcon = new User("Captain", "Falcon", "smoketwojoints@fucknintendo.org", "falcon", false, "images/midterm/cf.webp");
+const peach = new User("Princess", "Peach", "spintowin@fucknintendo.org", "peach", false, "images/midterm/peach.webp");
+const iceClimbers = new User("Ice.", "Climbers", "wobble@fucknintendo.org", "iceclimbers", false, "images/midterm/ics.webp");
+const pikachu = new User("Pikachu", "", "pikapika@fucknintendo.org", "pikachu", false, "images/midterm/pikachu.webp");
+const yoshi = new User("Yoshi", "", "dinoboy@fucknintendo.org", "yoshi", false, "images/midterm/yoshi.webp");
+const samus = new User("Samus", "Aran", "chargeshot@fucknintendo.org", "samus", false, "images/midterm/samus.png");
+const luigi = new User("Luigi", "", "brother2@fucknintendo.org", "luigi", false, "images/midterm/luigi.webp");
+const doctorMario = new User("Doctor", "Mario", "imarealdoctor@fucknintendo.org", "doc", false, "images/midterm/doc.webp");
+const ganondorf = new User("Ganondorf", "", "iwishiwascaptainfalcon@fucknintendo.org", "ganondorf", false, "images/midterm/ganon.webp");
+const mario = new User("Mario", "", "brother1@fucknintendo.org", "mario", false, "images/midterm/mario.png");
 
 // Array of Users
-let users = [dobbsB, mothersbaughM, nixonM, riftZ, mavridesP, reubensP, byrneD, crumbR, hoslerM, willsD, conheimP, leideckerJ, wilsonE, gaughB, nowellJ];
+let users = [fox, marth, jigglypuff, falco, sheik, captainFalcon, peach, iceClimbers, pikachu, yoshi, samus, luigi, doctorMario, ganondorf, mario];
 console.log(users);
 
 
@@ -67,31 +67,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Fixes issues where pressing enter causes form submission
     // makes it so login button is pressed instead
-    document.querySelector("#emailInput").addEventListener("keypress", function (event) {
+    document.querySelector("#usernameInput").addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
             event.preventDefault(); // Prevents form submission if inside a form
             document.querySelector("#loginBtn").click(); // Triggers login button click
         }
     });
-
-    // Fixes issues where pressing enter causes form submission
-    // makes it so login button is pressed instead
-    document.querySelector("#passwordInput").addEventListener("keypress", function (event) {
-        if (event.key === "Enter") {
-            event.preventDefault(); // Prevents form submission if inside a form
-            document.querySelector("#loginBtn").click(); // Triggers login button click
-        }
-    });
-
 });
 
 // Get text from modal and verify the user exists
 const loginBtn = document.querySelector("#loginBtn");
 loginBtn.addEventListener("click", () => {
-    const emailInput = document.querySelector("#emailInput").value;
-    const passwordInput = document.querySelector("#passwordInput").value;
+    const usernameInput= document.querySelector("#usernameInput").value;
     for (let i = 0; i < users.length; i++) {
-        if (users[i].email === emailInput && users[i].password === passwordInput) {
+        if (users[i].username === usernameInput) {
             login();
             break;
         }
@@ -99,9 +88,10 @@ loginBtn.addEventListener("click", () => {
 });
 
 function login() {
+    const row = document.querySelector("div#cards.row");
+    row.innerHTML = "";
     users.forEach(user => {
-        const row = document.querySelector("div#cards.row");
-        let cardDiv = document.createElement("div");
+        const cardDiv = document.createElement("div");
         cardDiv.classList.add("col", "m-3", "d-flex", "justify-content-center", "align-items-center");
         cardDiv.innerHTML = user.htmlCard;
         row.appendChild(cardDiv);
