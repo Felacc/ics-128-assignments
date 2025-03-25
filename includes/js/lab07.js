815941d88c4d22df4644c190bb26e81498c9a3f0
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-24 22:41:39 -0700

    Add loading animation

--- includes/js/lab07.js
+++ includes/js/lab07.js
@@ -1,20 +1,16 @@
+function loadWelcome() {
+    $("#welcomeDiv").removeClass("d-none").hide().show(1250).slideDown("slow");
+}
+
 function loadQuiz(name) {
     $("#welcomeDiv").hide();
     $("#testerName").text(name);
-    $("#quizContent").removeClass("d-none");
-    $("#quizContent").css("display", "block")
+    $("#quizContent").removeClass("d-none").hide().fadeIn(500);
 }
 
 $(document).ready(function () {
-    $("#quizContent").hide(); // ensure quiz is hidden
-
-    // Pressing enter while in the input field registers as a button click for the begin button
-    $("#name").keypress(function (event) {
-        if (event.which === 13) { // 13 is the Enter key
-            $("#begin").click(); // Trigger button click
-        }
-    });
-
+    loadWelcome();
+    
     $("#begin").click(function () {
         const name = $("#name").val().trim();
 
@@ -22,6 +18,13 @@ $(document).ready(function () {
             loadQuiz(name);
         } else {
             $("#errorMessage").html('<p class="text-warning fs-5 mt-3">Please enter your name :)</p>');
+        }
+    });
+
+    // Pressing enter while in the input field registers as a button click for the begin button
+    $("#name").keypress(function (event) {
+        if (event.which === 13) { // 13 is the Enter key
+            $("#begin").click(); // Trigger button click
         }
     });
 });
