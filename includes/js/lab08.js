972e9e4ba697ae344cc1efa6da66bd70716c2a14
Felacc <98856469+Felacc@users.noreply.github.com>
2025-03-31 21:23:11 -0700

    Add validation for dates and error message

--- includes/js/lab08.js
+++ includes/js/lab08.js
@@ -1,12 +1,12 @@
 $(function () {
-        $(".datepicker").datepicker();
-    
-} );
+    $(".datepicker").datepicker();
+
+});
 
 // Returns number of nights in the stay
 function getNumberOfNights(arrivalDate, departureDate) {
     const oneDay = 1000 * 60 * 60 * 24; // 1000 milliseconds in a second, 60 seconds in a minute, 60 minutes in an hour, 24 hours in a day
-    const nights = (departureDate - arrivalDate) / oneDay;
+    const nights = Math.round((departureDate - arrivalDate) / oneDay); // idk why but I was getting rounding errors over like 5 days
     return nights;
 }
 
@@ -15,13 +15,20 @@ $("#bookRoomBtn").on("click", function () {
     const arrivalDate = Date.parse($("#arrivalInput").val());
     const departureDate = Date.parse($("#departureInput").val());
     const nights = getNumberOfNights(arrivalDate, departureDate);
-    const pricePerNight = $("input[name='rooms']:checked").val(); // value attribute in HTML is set to the rooms price per night
-    const totalPrice = nights * pricePerNight;
-    const resultsHTML = `
-        <p class="text-white">Your length of stay is: ${nights} nights</p>
-        <p class="text-white">$${pricePerNight}/night</p>
-        <p class="text-white">Total: $${totalPrice}.00</p>
-    `
 
-    $("#results").html(resultsHTML);
+    if (nights < 1) {
+        $("#results").fadeOut(250);
+        $("#resultsError").hide().fadeIn(250).text("Must book at least 1 night");
+    } else {
+        $("#resultsError").fadeOut(250);
+        const pricePerNight = $("input[name='rooms']:checked").val(); // value attribute in HTML is set to the rooms price per night
+        const totalPrice = nights * pricePerNight;
+        const resultsHTML = `
+            <p class="text-white">Your length of stay is: ${nights} nights</p>
+            <p class="text-white">$${pricePerNight}/night</p>
+            <p class="text-white">Total: $${totalPrice}.00</p>
+        `
+
+        $("#results").hide().fadeIn(250).html(resultsHTML);
+    }
 });
